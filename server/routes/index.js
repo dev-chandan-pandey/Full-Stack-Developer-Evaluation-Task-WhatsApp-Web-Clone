877ff8888dd77d466deab484a4af727e9493d9ec
dev-chandan-pandey <iamchandanpandey11@gmail.com>
2025-08-10 21:50:10 +0530

    user auth is running

---
+++ server/routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const registerUser = require('../controller/registerUser')
+const checkEmail = require('../controller/checkEmail')
+const checkPassword = require('../controller/checkPassword')
+const userDetails = require('../controller/userDetails')
+const logout = require('../controller/logout')
+
+
+const router = express.Router()
+
+//create user api
+router.post('/register',registerUser)
+//check user email
+router.post('/email',checkEmail)
+//check user password
+router.post('/password',checkPassword)
+//login user details
+router.get('/user-details',userDetails)
+//logout user
+router.get('/logout',logout)
+
+
+
+module.exports = router
